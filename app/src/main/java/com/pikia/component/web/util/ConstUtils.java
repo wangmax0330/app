da5873efeba5bbb47c00d688d4b637d521515238
wangmax0330 <wangmax0330@gmail.com>
2015-11-15 13:39:11 +0800

    系统默认的一些常量封装

--- app/src/main/java/com/pikia/component/web/util/ConstUtils.java
+++ app/src/main/java/com/pikia/component/web/util/ConstUtils.java
@@ -6,6 +6,7 @@ public class ConstUtils {
 	public static final String PAGINATION_PAGE_KEY = "pageIndex";
 	public static final String PAGINATION_SORT_TYPE = "sortType";
 	public static final String PAGINATION_SORT_FIELD = "sortField";
+
 	public static final String SESSION_USER_KEY = "_session_user_key";
 	public static final String SESSION_USER_ID = "_session_user_id";
 	public static final String SESSION_DEFAULT_LOCALE = "zh_CN";
